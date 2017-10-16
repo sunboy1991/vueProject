ceb138c2010b9426fb68e58f253c58974f2320ba
luhaiguang <luhaiguang@111.com.cn>
2017-10-16 18:04:24 +0800

    路由器搭建

---
+++ src/components/header.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="">
+		我是头部00000
+	</div>
+	
+</template>
+
+<script>
+	export default {
+	  name: 'HelloWorld',
+	  data () {
+	    return {
+	      msg: 'Welcome to Your Vue.js App'
+	    }
+	  }
+	}	
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import header from '@/components/header'
 
 Vue.use(Router)
 
@@ -9,7 +10,14 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: HelloWorld
-    }
+      component: HelloWorld,
+      children: [
+        {
+          path: 'header',
+          component: header
+        }
+
+  ]
+    }  
   ]
 })
