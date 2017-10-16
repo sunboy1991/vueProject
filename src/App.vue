97e2199c7ee24ab9e180d8fa7436081467501857
sunboy1991 <1044768335@qq.com>
2017-10-16 20:46:23 +0800

    发送ajax

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
-  <div id="app">
-  <router-view></router-view>
-  </div>
+	<div id="app">
+		<router-view></router-view>
+		<div class="data">
+			{{msg}}
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+	export default {
+		name: 'app',
+		data() {
+			return {
+				msg: 'Welcome to Your Vue.js App'
+			}
+		},
+		created: function () {
+			var url = 'http://m.yaoex.com/manage/api/mobileIndex/listIndexFloor';
+			this.$http.post(url).then(function(data) {
+				var json = data.body;
+				this.msg = eval("(" + json + ")");
+			}, function(response) {
+				console.log(response)
+			})
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
